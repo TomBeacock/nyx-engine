102718355bbf3031c5e79d4e0397831502d3d6e1
Thomas Beacock <thomasbeacock@gmail.com>
2024-05-09 12:21:14 +0100

    Match formatted output of log

--- engine/include/nyx/assert.h
+++ engine/include/nyx/assert.h
@@ -3,10 +3,16 @@
 #include "log.h"
 
 #ifdef NYX_ASSERTS_ENABLED
-#define NYX_ASSERT(check, message, ...)                                        \
+#define NYX_ASSERT(check, msg)                                                 \
     {                                                                          \
         if (!(check)) {                                                        \
-            NYX_ERROR(message __VA_OPT__(, ) __VA_ARGS__);                     \
+            NYX_ERROR(msg);                                                    \
+        }                                                                      \
+    }
+#define NYX_ASSERT_F(check, fmt, ...)                                          \
+    {                                                                          \
+        if (!(check)) {                                                        \
+            NYX_ERROR_F(fmt __VA_OPT__(, ) __VA_ARGS__);                       \
         }                                                                      \
     }
 #else
